d54e2a2a0d47b190f3d6850c82141dab6bb8bc7f
gokarna123-goku <agokarna444@gmail.com>
2023-07-22 10:25:46 +0545

    created new file and edited existing files

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          It's Gokarna Adhikari, To do list app.
-        </p>
-      </header>
+    <div>
+      Hello there,
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>This is Header Section </div>
+  )
+}
+
+export default Header
