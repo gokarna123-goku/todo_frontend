599bdd0723e0329c34204e81c3a57eec42370362
gokarna123-goku <agokarna444@gmail.com>
2023-07-22 20:30:37 +0545

    initialized the redux

---
+++ src/redux/store.js
@@ -0,0 +1,18 @@
+import { combineReducers, createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+const reducer = combineReducers({
+  todos: todosReducer,
+});
+
+const middleware = [thunk];
+
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
+export default store;
+
+//
