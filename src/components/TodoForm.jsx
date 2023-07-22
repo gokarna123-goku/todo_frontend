1b857f027c55aa9d3833486510f55ba1b7d88ed3
gokarna123-goku <agokarna444@gmail.com>
2023-07-22 10:58:57 +0545

    working on the form section

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 
 const TodoForm = () => {
+  // Form Submit
+  const onFormSubmit = () => {
+    const [text, setText] = useState("");
+  };
+
+  //   value submit
+  const onInputSubmit = (e) => {
+    setText(e.target.value);
+  };
+
   return (
     <>
-      <form className="todo_form">
-        <input type="text" placeholder="Enter new todo" className="todo_input" />
+      <form className="todo_form" onSubmit={onFormSubmit}>
+        <input
+          type="text"
+          placeholder="Enter new todo"
+          className="todo_input"
+          value={text}
+          onChange={onInputSubmit}
+        />
       </form>
     </>
   );
