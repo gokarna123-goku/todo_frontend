ddc0adaa5a9e224a965621fb76c9a668766df0cf
gokarna123-goku <agokarna444@gmail.com>
2023-07-22 20:37:03 +0545

    modified and created file

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { combineReducers, createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
+import { todosReducer } from "./reducers/todosReducer";
+
 const reducer = combineReducers({
   todos: todosReducer,
 });
