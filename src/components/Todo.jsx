7377e49c12208394171f115bce03e286c2a5116e
gokarna123-goku <agokarna444@gmail.com>
2023-07-24 17:08:44 +0545

    done for today

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,8 +1,16 @@
 import React from "react";
+import { toggleTodo } from "../redux/actions/index";
+
+import { useDispatch } from "react-redux";
 
 const Todo = ({ todo }) => {
+  const dispatch = useDispatch();
   return (
-    <li className="task">
+    <li
+      className="task"
+      onClick={() => dispatch(toggleTodo(todo._id))}
+      style={{ background: todo.done ? "#50C878" : "#EE4B2B" }}
+    >
       <p className="task_name">{todo.data}</p>
 
       <div className="actions">
--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { ADDNEW_TODO, GETALL_TODO } from "./type";
+import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO } from "./type";
 
 const API_URL = "http://localhost:8000";
 
@@ -19,6 +19,17 @@ export const getAllTodos = () => async (dispatch) => {
   try {
     const res = await axios.get(`${API_URL}/todos`);
     dispatch({ type: GETALL_TODO, payload: res.data });
+  } catch (error) {
+    console.log("Error while calling getAllTodos API ", error.message);
+  }
+};
+
+
+// toggle todos
+export const toggleTodo = (id) => async (dispatch) => {
+  try {
+    const res = await axios.get(`${API_URL}/todos/${id}`);
+    dispatch({ type: TOGGLE_TODO, payload: res.data });
   } catch (error) {
     console.log("Error while calling getAllTodos API ", error.message);
   }
