76c4833767f3a3319c8210a776b80726a8ec538b
gokarna123-goku <agokarna444@gmail.com>
2023-07-24 14:35:45 +0545

    working on the displaying the all todos

--- src/redux/actions/index.js
+++ src/redux/actions/index.js
@@ -4,11 +4,24 @@ import { ADDNEW_TODO } from "./type";
 
 const API_URL = "http://localhost:8000";
 
-export const addNewTodo = (data) => async(dispatch) => {
+// Adding new todo
+export const addNewTodo = (data) => async (dispatch) => {
   try {
     const res = await axios.post(`${API_URL}/todos`, { data });
-    dispatch({type: ADDNEW_TODO, payload: res.data});
+    dispatch({ type: ADDNEW_TODO, payload: res.data });
   } catch (error) {
     console.log("Error while calling addNewTodo API ", error.message);
   }
 };
+
+// Fetching all todos
+export const getAllTodos = (data) => async (dispatch) => {
+  try {
+    const res = await axios.post(`${API_URL}/todos`);
+    // dispatch({ type: ADDNEW_TODO, payload: res.data });
+  } catch (error) {
+    console.log("Error while calling getAllTodos API ", error.message);
+  }
+};
+
+//
