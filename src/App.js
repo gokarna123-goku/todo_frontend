456dd479f13ad6570aee708195dd9d2089fdb3fd
gokarna123-goku <agokarna444@gmail.com>
2023-07-24 14:23:23 +0545

    creating new file for fetching all todos task

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
 import TodoForm from "./components/TodoForm";
+import Todos from "./components/Todos";
 
 function App() {
   return (
     <>
       <Header />
       <TodoForm />
+      <Todos />
     </>
   );
 }
---
+++ src/components/Todos.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Todos = () => {
+  return (
+    <>
+      <div>Hello all todos task.</div>
+    </>
+  );
+};
+
+export default Todos;
